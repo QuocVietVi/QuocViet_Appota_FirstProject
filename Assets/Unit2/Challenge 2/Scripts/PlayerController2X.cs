03bfce289fdd27e5b98ffab3346e4d83be5a6cdf
minihwn <vietvqgch210657@fpt.edu.vn>
2024-06-19 21:25:36 +0700

    Unit 2 (Challenge2)

---
+++ Assets/Unit2/Challenge 2/Scripts/PlayerController2X.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController2X : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    bool canSpawn = true;
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && canSpawn)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            canSpawn = false;
+            Invoke(nameof(DelaySpawn), 1f);
+        }
+    }
+
+    void DelaySpawn()
+    {
+        canSpawn = true;
+    }
+}
